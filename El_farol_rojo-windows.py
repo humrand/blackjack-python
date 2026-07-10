ff2b0b83a081ada802276f1eb8cad66c44bbc4de
humrand <oh2063365@gmail.com>
2026-07-11 00:44:03 +0200

    Update version from 1.5.4 to 1.6.5

--- El_farol_rojo-windows.py
+++ El_farol_rojo-windows.py
@@ -139,7 +139,7 @@ def TR(key):
 pygame.init()
 pygame.mixer.init()
 
-VERSION = "1.5.4"
+VERSION = "1.6.5"
 GITHUB_RAW_URL  = "https://raw.githubusercontent.com/humrand/blackjack-python/main/El_farol_rojo.py"
 GITHUB_API_URL  = "https://api.github.com/repos/humrand/blackjack-python/commits?path=El_farol_rojo.py&per_page=1"
 GITHUB_RAW_BASE = "https://raw.githubusercontent.com/humrand/blackjack-python/{sha}/El_farol_rojo.py"
